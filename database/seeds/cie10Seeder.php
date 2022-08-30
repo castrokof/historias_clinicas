ec6e311bc97cf45fc9a04600e9b9753d041abc1c
Carlos Bejarano <cabejarano21@gmail.com>
2022-08-30 14:58:00 -0500

    feat(seeders): Se creo el seeder de ciudades, paises, departamentos, ocupaciones, especialidades

--- database/seeds/cie10Seeder.php
+++ database/seeds/cie10Seeder.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Models\Admin\Cie10;
-use Carbon\Carbon;
+// use Carbon\Carbon;
 use App\Models\Admin\Paises;
 use App\Models\Admin\Ciudades;
+use App\Models\Admin\Departamentos;
+use App\Models\Admin\Ocupaciones;
+use App\Models\Admin\Def_Especialidades;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -48,6 +51,42 @@ class cie10Seeder extends Seeder
         foreach ($data as $obj) {
             Ciudades::create(array(
                 'cod_ciudad' => $obj->cod_ciudad,
+                'nombre' => $obj->nombre,
+                'estado' => 1
+
+            ));
+        }
+
+        DB::table('departamentos')->delete();
+        $json = File::get('database/data/departamentos.json');
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Departamentos::create(array(
+                'cod_departamento' => $obj->cod_departamento,
+                'nombre' => $obj->nombre,
+                'estado' => 1
+
+            ));
+        }
+
+        DB::table('ocupaciones')->delete();
+        $json = File::get('database/data/ocupaciones.json');
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Ocupaciones::create(array(
+                'codigo' => $obj->codigo,
+                'nombre' => $obj->nombre,
+                'estado' => 1
+
+            ));
+        }
+
+        DB::table('def__especialidades')->delete();
+        $json = File::get('database/data/especialidades.json');
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Def_Especialidades::create(array(
+                'codigo' => $obj->codigo,
                 'nombre' => $obj->nombre,
                 'estado' => 1
 
